3dc503f205d064fb49f8f3024fc908ec6ec7aad1
DavidFonsecaG <davidfonsecagaona@gmail.com>
2022-04-21 22:51:56 -0500

    First Changes HomePage

--- frontend-client/src/pages/home/HomePage.jsx
+++ frontend-client/src/pages/home/HomePage.jsx
@@ -3,19 +3,9 @@ import '../../scss/home.scss';
 
 const HomePage = () => {
   return (
-    <div className="container container-1200 container-flex">
+    <div className="container-flex">
 
-      <section className="container-section">
-        <article className="article-featured">
-          <h2 className="article-title">Finding simplicity in life</h2>
-          <img src="img/life.jpg" alt="simple white desk on a white wall with a plant on the far right side" className="article-image" />
-          <p className="article-info">July 23, 2019 | 3 comments</p>
-          <p className="article-body"><strong>Life can get complicated really quickly</strong>, but it doesn't have to be! There are many ways to simplify your life, <a href="#">a few of which we've explored in the past</a>. This week we're taking a bit of a approach though, in how you can find simplicity in the life you already living.</p>
-          <a href="#" className="article-read-more">CONTINUE READING</a>
-        </article>
-      </section>
-
-      <main className="container-main">
+      <main className="container container-1300 container-main">
 
         <article className="article-featured">
           <h2 className="article-title">Finding simplicity in life</h2>
@@ -63,32 +53,6 @@ const HomePage = () => {
         </article>
 
       </main>
-
-      <aside className='container-sidebar'>
-
-        <div className="sidebar-widget">
-          <h2 className="widget-title">ABOUT ME</h2>
-          <p className="widget-body">I find life better, and I'm happier, when things are nice and simple.</p>
-          <img src="img/about-me.jpg" alt="john doe" className="widget-image" />
-        </div>
-
-        <div className="sidebar-widget">
-          <h2 className="widget-title">RECENT POSTS</h2>
-          <div className="widget-recent-post">
-            <img src="img/food.jpg" alt="two dumplings on a wood plate with chopsticks" className="widget-image" />
-            <h3 className="widget-recent-post-title">Keeping cooking simple</h3>
-          </div>
-          <div className="widget-recent-post">
-            <img src="img/work.jpg" alt="a chair white chair at a white desk on a white wall" className="widget-image" />
-            <h3 className="widget-recent-post-title">Simplicity and work</h3>
-          </div>
-          <div className="widget-recent-post">
-            <img src="img/deco.jpg" alt="a green plant in a clear, round vase with water in it" className="widget-image" />
-            <h3 className="widget-recent-post-title">Simple decorations</h3>
-          </div>
-        </div>
-
-      </aside>
       
     </div>
   );
